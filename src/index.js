7b5e57424b77519d96e712a5fad98ad85211bfad
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-01-22 16:26:41 +0000

    Add player form event listener

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const gameboard = (() => {
   const addPlayer1Score = score[0];
   const addPlayer2Score = score[1];
   return {
-    getScore, 
-    getSize, 
-    addPlayer1Score, 
+    getScore,
+    getSize,
+    addPlayer1Score,
     addPlayer2Score
   };
 })();
@@ -19,3 +19,19 @@ const playerFactory = (name, team) => {
     team
   };
 };
+
+const gameBoard = document.querySelector('.gameboard');
+const cell = gameBoard.querySelectorAll('.cell');
+const playerForm = document.querySelector('form');
+
+playerForm.addEventListener(
+  'submit',
+  (e) => {
+    const data = new FormData(playerForm);
+    const playerName = data.get('player-name');
+    const playerTeam = data.get('team');
+    const player = playerFactory(playerName, playerTeam);
+    e.preventDefault();
+  },
+  false
+);
