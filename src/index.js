6338966de389f2acf8fe860f2bc6531b7deabf77
Samuel Evans <117552758+JSamuel38@users.noreply.github.com>
2023-01-22 15:36:28 +0000

    Add gameboard object

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const gameboard = (() => {
+  const size = new Array(9);
+  const score = [0, 0];
+  const getScore = score;
+  const getSize = size;
+  const addPlayer1Score = score[0];
+  const addPlayer2Score = score[1];
+  return {
+    getScore, 
+    getSize, 
+    addPlayer1Score, 
+    addPlayer2Score
+  };
+})();
